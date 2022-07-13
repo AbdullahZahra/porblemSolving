dcfa37e85f33c1789676d833541f8d97ae97be30
Abdullah Zahra <abdullahmzahra@gmail.com>
2022-07-13 09:44:42 +0200

    1523. Count Odd Numbers in an Interval Range

---
+++ leetcode1523.php
@@ -0,0 +1,21 @@
+/**
+    Problem 1523 LeetCode
+    1523. Count Odd Numbers in an Interval Range
+    https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/
+*/
+
+class Solution {
+
+/**
+ * @param Integer $low
+ * @param Integer $high
+ * @return Integer
+ */
+function countOdds($low, $high) {
+    $counter = 0;
+    $counter = ($high - $low)/2;
+    if ($low % 2 == 1 || $high % 2 == 1) $counter++;
+
+    return floor($counter);
+}
+}
